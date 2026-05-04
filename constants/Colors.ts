83a480fc0b47a1b7ea8a5190ee2f1ba4384a94f2
president-sketch <president@pmigc.net>
2026-05-04 11:19:37 +0300

    Add constants/Colors.ts

---
+++ constants/Colors.ts
@@ -0,0 +1,9 @@
+export const Colors = {
+  primary: '#D13434',
+  primaryDark: '#a02020',
+  dark: '#1a0505',
+  accent: '#e6557c',
+  white: '#ffffff',
+  lightBg: '#fff5f5',
+  border: '#fce8e8',
+};
